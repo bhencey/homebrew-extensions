8db09cfdbfdf95c5433199228dc908bac3909958
Brandon Hencey <brandon.hencey@us.af.mil>
2022-06-03 21:57:41 -0400

    setup_cac formula

---
+++ Formula/setup_cac.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class SetupCac < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bhencey/homebrew-extensions/archive/refs/tags/setup_cac_0.1.tar.gz"
+  sha256 "1aef7c9819501a2520da7ea64668e895ab2aa61c05d8afc5ccb2520c90ce8d11"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "setup_cac"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-extensions`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
