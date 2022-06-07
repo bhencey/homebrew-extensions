0af0e302cfee7b53fec21b08f9064655eaf3d981
Brandon Hencey <brandon.hencey@us.af.mil>
2022-06-07 07:23:16 -0400

    added pkcs11 engine tests * test existence of opensc module in p11-kit * test availability of pkcs11 module in openssl * test existence of pkcs11 module in curl

--- cmd/config_ssl.rb
+++ cmd/config_ssl.rb
@@ -16,6 +16,17 @@ module Homebrew
     
     puts "Setup openssl with pkcs11 engine"
     system "sh #{cmd_path}/setup_openssl.sh #{HOMEBREW_PREFIX}"
+
+    if !`p11-kit list-modules`.chomp.match(/opensc/)
+      puts "p11-kit missing opensc module"
+    end
+    if !`openssl engine pkcs11 -t`.chomp.match(/available/)
+      puts "openssl missing pkcs11 engine"
+    end
+    if !`curl --engine list`.chomp.match(/pkcs11/)
+      puts "curl missing pkcs11 engine"
+    end
+
    end
 
 end
