c215b60cc3cc31acf6d12928c53b6afdd1c566f5
Brandon Hencey <brandon.hencey@us.af.mil>
2022-06-16 12:22:51 -0400

    refined install documentation * fixed audit issues for git-config-cac.rb * added post install tests for git-config-cac.rb * refined up front readme documentation

--- Formula/git-config-cac.rb
+++ Formula/git-config-cac.rb
@@ -1,32 +1,26 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class GitConfigCac < Formula
-  desc ""
-  homepage ""
-  #url "https://github.com/bhencey/homebrew-extensions/archive/refs/tags/setup_cac_0.4.tar.gz"
+  desc "Setup CAC authentication over https for Git"
+  homepage "https://weaponone.test.cce.af.mil/"
   url "https://gitlab.rdte.afrl.dren.mil/henceybm/homebrew-extensions/-/archive/setup_cac_0.4/homebrew-extensions-setup_cac_0.4.tar.gz"
-  sha256 "ed3c77d5603e534ac62fca5dc674772d3c3c031f8318e5c726937c2fef9383ea"
+  sha256 "1cde121a2cf2fada1c8ac5a6a54b11989b9b20a10cab545da8a4d73481438c88"
   license ""
-  
-  depends_on "opensc"
-  depends_on "gnutls"
+
   depends_on "git-https-cac"
+  depends_on "gnutls"
+  depends_on "opensc"
 
   def install
     bin.install "git-config-cac"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-extensions`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert `which git | grep "$(brew --prefix)"`!=nil, "Using brewed git"
+    assert `p11-kit list-modules`.chomp.match(/opensc/), "OpenSC module installed for p11-kit"
+    assert `openssl engine pkcs11 -t`.chomp.match(/available/), "OpenSSL has pkcs11 engine"
+    assert `curl --engine list`.chomp.match(/pkcs11/), "curl sees OpenSSL's pkcs11 engine"
+    # puts "Setup p11-kit with opensc module"
+    # system "echo module: #{HOMEBREW_PREFIX}/Cellar/opensc/*/lib/opensc-pkcs11.so >#{HOMEBREW_PREFIX}/etc/pkcs11/modules/opensc.module"
+    
+    # system "false"
   end
 end
---
+++ ISSUES.md
@@ -0,0 +1,14 @@
+# Known Issues and TODOs
+
+## Install and Uninstall
+* Script/command to remove pkcs11 engine from openssl configuration.
+* Script/command to remove opensc module from pk11-kit.
+
+## Error Handling
+* Detect when CAC not found.
+* Detect when PIV authentication certificate not found.
+
+## Diagnostics
+
+## Features
+
