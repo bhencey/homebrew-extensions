e541191c04e80f088f5a5ec468ae37a46d469a79
Brandon Hencey <brandon.hencey@us.af.mil>
2022-07-07 11:02:15 -0400

    updated style for setup_openssl brew command

--- cmd/setup_openssl.rb
+++ cmd/setup_openssl.rb
@@ -3,16 +3,16 @@
 module Homebrew
   module_function
 
-  def setup_openssl_args
-  end
-  
+  def setup_openssl_args; end
+
   def setup_openssl
     cmd_path = __dir__
 
     puts "Setup p11-kit with opensc module"
-    system "echo module: $(brew --prefix opensc)/lib/pkcs11/opensc-pkcs11.so > $(brew --prefix)/etc/pkcs11/modules/opensc.module"
+    system  "echo module: $(brew --prefix opensc)/lib/pkcs11/opensc-pkcs11.so > " \
+            "$(brew --prefix)/etc/pkcs11/modules/opensc.module"
 
     puts "Setup openssl with pkcs11 engine"
-    system "sh #{cmd_path}/setup_openssl.sh"
+    system "sh", "#{cmd_path}/setup_openssl.sh"
   end
 end
