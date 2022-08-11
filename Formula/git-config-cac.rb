bbecbbc7c785b71b751bf85a0dbf824abfcf8210
Brandon Hencey <brandon.hencey@us.af.mil>
2022-08-11 19:20:38 -0400

    updated with new version of git-config-cac

--- Formula/git-config-cac.rb
+++ Formula/git-config-cac.rb
@@ -1,8 +1,8 @@
 class GitConfigCac < Formula
   desc "Setup CAC authentication over https for Git"
   homepage "https://weaponone.test.cce.af.mil/"
-  url "https://github.com/bhencey/homebrew-extensions/archive/refs/tags/setup_cac_0.5.3.tar.gz"
-  sha256 "403ceab3b61e229b52b61159a81a033be7a74b94c7c5d47cdd487006ce72d3cf"
+  url "https://github.com/bhencey/homebrew-extensions/archive/refs/tags/setup_cac_0.5.4.tar.gz"
+  sha256 "9baa96aac9934b3660d9aaecbf5b93436918ae6bc2a6768b7b09f48cdc26890c"
   license ""
 
   depends_on "git-https-cac"
